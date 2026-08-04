86fbce9131859ceff3f3da11acea1013fbfb3f70
Stefan Grosser <13567009+Bierchermuesli@users.noreply.github.com>
2026-08-04 23:13:13 +0200

    Bump version to 1.0.4
    Ships MAC/ARP History, the Default interface view with live-refresh,
    and the From/To date-range pickers added this round.

--- netbox_lens/__init__.py
+++ netbox_lens/__init__.py
@@ -5,7 +5,7 @@ class LensConfig(PluginConfig):
     name = "netbox_lens"
     verbose_name = "LENS"
     description = "Locate Endpoints across Network Systems"
-    version = "1.0.3"
+    version = "1.0.4"
     author = "Stefan Grosser"
     author_email = "stefan.grosser@zebbra.ch"
     base_url = "lens"
