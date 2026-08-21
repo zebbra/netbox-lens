4ea10916d8abb1d55ad4c1798a0e504d57d5e653
Stefan Grosser <13567009+Bierchermuesli@users.noreply.github.com>
2026-08-21 13:46:28 +0200

    Bump version to 1.0.12

--- netbox_lens/__init__.py
+++ netbox_lens/__init__.py
@@ -5,7 +5,7 @@ class LensConfig(PluginConfig):
     name = "netbox_lens"
     verbose_name = "LENS"
     description = "Locate Endpoints across Network Systems"
-    version = "1.0.11"
+    version = "1.0.12"
     author = "Stefan Grosser"
     author_email = "stefan.grosser@zebbra.ch"
     base_url = "lens"
